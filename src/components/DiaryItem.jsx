cffd1e32f96a9af6eb791e96ef6edfb580ab45a3
DongEun02 <ehddms0517@naver.com>
2025-04-03 22:47:30 +0900

    Viewer update

--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -2,9 +2,13 @@ import "./DiaryItem.css";
 import { getEmotionImage } from "../util/get-emotion-image";
 import Button from "./Button";
 import { useNavigate } from "react-router-dom";
+import { weatherList } from "../util/weather-const";
 
-const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+const DiaryItem = ({ id, emotionId, createdDate, weatherId, content }) => {
   const nav = useNavigate();
+  const weatherItem = weatherList.find(
+    (item) => String(item.weatherId) === String(weatherId)
+  );
 
   return (
     <div className="DiaryItem">
@@ -20,6 +24,8 @@ const DiaryItem = ({ id, emotionId, createdDate, content }) => {
         </div>
         <div className="content">{content}</div>
       </div>
+      <div className="weather_section">날씨: {weatherItem.weatherName}</div>
+
       <div className="button_section">
         <Button onClick={() => nav(`/edit/${id}`)} text={"수정하기"} />
       </div>
